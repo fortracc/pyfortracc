8ee5ccdedf2ed3d3f0f0f33df8302e6ef792d0e9
Helvecio Neto <helvecio.neto@inpe.br>
2024-11-07 09:42:47 -0300

    Update _init_ text

--- pyfortracc/__init__.py
+++ pyfortracc/__init__.py
@@ -3,7 +3,7 @@ pyfortracc
 =====
 
 Provides
-    1. Tracking Hydrological Non-Rigid Clusters in 2D matrix
+    1. Tracking Non-Rigid Clusters in 2D matrix
     2. Forecasting the movement of the Clusters by extrapolation
     3. Plot and analysis of the results of the tracking and forecasting
     4. Visualizing the results of the tracking, validation and forecasting
@@ -18,7 +18,7 @@ with the code, and a reference guide, available from
 Available modules
 ---------------------
 track
-    Tracking Hydrological Non-Rigid Clusters in 2D matrix
+    Tracking Non-Rigid Clusters in 2D matrix
 forecast
     Forecasting the movement of the Clusters by extrapolation
 
@@ -39,20 +39,19 @@ plot
 
 About the package
 -----------------------------
-pyForTraCC is a Python package designed to identify, track, and analyze hydrological features in various types of datasets.
+pyForTraCC is a Python package designed to identify, track, forecast and analyze clusters moving in a time-varying field.
 It offers a modular framework that incorporates different algorithms for feature identification, tracking, and analyses.
 One of the key advantages of pyForTraCC is its versatility, as it does not depend on specific input variables or a particular grid structure.
-In its current implementation, pyForTraCC identifies individual hydrological features by locating maxima or minima in a two-dimensional time-varying field.
+In its current implementation, pyForTraCC identifies individual cluster features in a 2D field by applying a specified threshold value.
 
 By utilizing a time-varying 2D input images and a specified threshold value, pyForTraCC can determine the associated volume for these features. The 
 software then establishes consistent trajectories that represent the complete lifecycle of a single cell of feature through the tracking step. 
 Furthermore, pyForTraCC provides analysis and visualization methods that facilitate the utilization and display of the tracking results.
 
 This algorithm was initially developed and used in the publication "Impact of Multi-Thresholds and Vector Correction for Tracking Precipitating 
-Systems over the Amazon Basin" (https://doi.org/10.3390/rs14215408). The methods presented in the research paper "Impact of Multi-Thresholds and Vector 
-Correction for Tracking Precipitating Systems over the Amazon Basin" have enabled the implementation of robust techniques for extracting the motion vector 
-field and trajectory of individual clusters of precipitating cells. These techniques play a crucial role in accurately identifying and analyzing precipitation 
-systems.
+Systems over the Amazon Basin" (https://doi.org/10.3390/rs14215408). The methods presented in the research paper have enabled the implementation of robust techniques for extracting the motion vector 
+field and trajectory of individual clusters of precipitating cells. These techniques have been applied to the Amazon Basin, where the tracking of 
+precipitating systems is essential for understanding the hydrological cycle and its impacts on the environment and used in this algorithm
 
 For further information on pyForTraCC, its modules, and the continuous development process, please refer to the official documentation and stay tuned for updates 
 from the community.
