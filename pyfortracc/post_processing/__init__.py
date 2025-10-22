f435d295415e1b4b2633288ee67f0173ad80f254
Helvecio Neto <helvecio.neto@inpe.br>
2025-10-22 11:28:16 -0300

    Adds add_vector_data function to integrate vector data into trace files based on spatial intersections.

--- pyfortracc/post_processing/__init__.py
+++ pyfortracc/post_processing/__init__.py
@@ -2,3 +2,4 @@ from .duration import compute_duration
 from .fortracc_converter import convert_parquet_to_family
 from .spatial_vectors import spatial_vectors
 from .add_raster_data import add_raster_data
+from .add_vector_data import add_vector_data
